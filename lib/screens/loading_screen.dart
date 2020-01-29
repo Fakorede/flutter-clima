c37eb9111aff27e7d93e510697dc60a473fe6577
fakorede <abiolafakorede@gmail.com>
2020-01-29 14:33:22 +0100

    passing weather data to the LocationScreen state object

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -25,21 +25,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     await location.getCurrentLocation();
 
-    latitude = location.latitude;
-    longitude = location.longitude;
-
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
 
     var weatherData = await networkHelper.getData();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen();
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
-
-    // double temperature = decodedData['main']['temp'];
-    // int condition = decodedData['weather'][0]['id'];
-    // String cityName = decodedData['name'];
   }
 
   @override
